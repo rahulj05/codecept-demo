5a38a282cfac2432fba4d8c34314b215202e7712
Aaron Humerickhouse <aaron@humerickhouse.me>
2020-12-10 21:22:17 -0600

    Tags

--- tests/acceptance/example-test.js
+++ tests/acceptance/example-test.js
@@ -5,20 +5,11 @@ Scenario('first user can click Google Search', ({ firstUser }) => {
     firstUser.setsSearch('codeceptjs')
     firstUser.clicksGoogleSearch()
     firstUser.sees('codecept.io')
-});
+}).tag('smoke');
 
 Scenario('second user can click Feeling Lucky', ({ secondUser }) => {
     secondUser.goesToPage('http://www.google.com/');
     secondUser.setsSearch('codeceptjs')
     secondUser.clicksFeelingLucky()
     secondUser.seesInCurrentUrl('https://codecept.io')
-});
-
-Scenario('second user cannot click Google Search', ({ secondUser }) => {
-    secondUser.goesToPage('http://www.google.com/');
-    secondUser.setsSearch('codeceptjs')
-    try {
-        secondUser.clicksGoogleSearch()
-    } catch (error) {
-    }
 });
